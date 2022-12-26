71220d0d5c89931ddf66c9c8fe8728362e04ed7c
sumitradas94 <sumitradas94@gmail.com>
2022-12-26 16:20:56 +0530

    daily Employee wage

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -11,18 +11,26 @@ namespace EmployeeWageproblem
         public static void CheckAttendance()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+           
             Random random = new Random();
             int empCheck = random.Next(2);
 
             if(empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empWage = 0;
             }
-
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" +empWage);
 
         }
     }
