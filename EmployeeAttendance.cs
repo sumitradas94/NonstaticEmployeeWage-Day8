781d8f64ad830dafc9e2d5988899a834b7cfd569
sumitradas94 <sumitradas94@gmail.com>
2022-12-27 11:14:57 +0530

    Switch case statement

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -10,27 +10,48 @@ namespace EmployeeWageproblem
     {
         public static void CheckAttendance()
         {
-            int IS_FULL_TIME = 1;
+           const int IS_FULL_TIME = 1;
+           const int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
-           
-            Random random = new Random();
-            int empCheck = random.Next(2);
 
-            if(empCheck == IS_FULL_TIME)
+            
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            switch(empCheck)
             {
-                Console.WriteLine("Employee is present");
-                empHrs = 8;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                    case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                    default: 
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                empWage = 0;
-            }
+
+
+          //  if(empCheck == IS_FULL_TIME)
+            //{
+             //   Console.WriteLine("Employee is present");
+                //empHrs = 8;
+           // }
+           //else if (empCheck == 2)
+           // {
+              //  Console.WriteLine("Employee is parttime");
+              //  empHrs = 4;
+          //  }
+           // else
+            //{
+              //  Console.WriteLine("Employee is absent");
+                //empWage = 0;
+           // }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage :" +empWage);
+            Console.WriteLine("Emp Wage :" + empWage);
 
         }
     }
