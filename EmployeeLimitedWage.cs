38882ce4072f2f923abdeab5d9dd00b4848b86e0
sumitradas94 <sumitradas94@gmail.com>
2022-12-27 18:01:16 +0530

    Assume 100 hours in 20 days

---
+++ EmployeeLimitedWage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageproblem
+{
+    public class EmployeeLimitedWage
+    {
+        public static void Limitwage()
+        {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAYS = 20;
+            const int MAX_HRS_IN_MONTH = 100;
+
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpHrs = 0;
+            int totalEmpwage = 0;
+            int totalWorkingDays =0;
+
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case 0:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#: " + totalWorkingDays + "Emp Hrs : " + totalEmpHrs); 
+
+
+
+        }
+
+            
+
+        }
+    }
+}
+
+        
+        
