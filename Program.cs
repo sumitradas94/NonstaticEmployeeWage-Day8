3ac63ca705b840fa3f7fce779acc1958473aeee4
sumitradas94 <120413479+sumitradas94@users.noreply.github.com>
2022-12-27 20:09:37 +0530

    Modified

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
     public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employeewage computation program");
+            EmployeeAttendance.CheckAttendance();
+            //Employeeday.daywage();
         }
     }
 
